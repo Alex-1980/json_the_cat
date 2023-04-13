cbd1bb36fdea9f184a411929e26b922cd0d4b98b
Chansoo Han <devhan1207@gmail.com>
2023-04-13 10:05:15 -0400

    catFetcher

---
+++ breedFetcher.js
@@ -0,0 +1,15 @@
+const name = process.argv.slice(2);
+
+const request = require('request');
+request(`https://api.thecatap.com/v1/breeds/search?q=${name}`, function(error, response, body) {
+  if(!body[name]) {
+    console.error('error:', "Not Found!")
+  }
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
+  console.log(typeof body);
+
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
+});
