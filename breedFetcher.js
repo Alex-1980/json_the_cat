416b0b31f094115e0cba603aefdb0df8709131f5
Chansoo Han <devhan1207@gmail.com>
2023-04-13 23:35:14 -0400

    breedFetcher with test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,24 @@
-const name = process.argv.slice(2);
-
 const request = require('request');
-request(`https://api.thecatap.com/v1/breeds/search?q=${name}`, function(error, response, body) {
-  if (!body[name]) {
-    console.error('error:', "Not Found!");
-  }
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
-  console.log(typeof body);
 
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-});
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return null;
+    }
+    if (response.statusCode !== 200) {
+      callback(`Status Code ${response.statusCode}`, null);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback(error, 'breed not found');
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
